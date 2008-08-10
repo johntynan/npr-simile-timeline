9c6e0e7036747f2f78b7b26eb2cbe4a32b12996c
jgtynan <jgtynan@a4e6c4df-f953-0410-8960-4529ec23a526>
2008-08-10 08:48:58 +0000

    Added the test file newsample.html as well as the first draft of the Query Generator Lite qglite.html.  Additionally, I think timeline_new.php actually makes the query to NPR sing javascript alone.  Added a link to the home page to point to the new svn repository, as well as a link to the new sample timeline.

---
+++ trunk/timeline_new.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html lang="en">
+<head>
+<title>SIMILE Timeline of Stories from NPR</title>
+
+<script src="http://simile.mit.edu/timeline/api/timeline-api.js" type="text/javascript"></script>
+<script src="http://johntynan.com/scripts/timeline/nprtimeline.js" type="text/javascript"></script>
+
+</head>
+<body onresize="onResize();" >
+<div id="my-timeline" class="timeline-default" style="height: 350px;">
+
+<script type="text/javascript" language="JavaScript">
+document.write('<sc'+'ript');
+document.write(' type="text/javascript"');
+document.write(' language="JavaScript"');
+document.write(' src="'+ parent.topFrame.document.selectionForm.queryBox.value +'&apiKey=MDAxNzgwMDQ5MDEyMTQ4NzYyMjU4YmY1Yw004&output=JSON&callback=onLoad">');
+document.write('</sc'+'ript>');
+</script>
+
+<?php  
+/*
+    $queryString=$_POST['apicall']; //The URL submitted 
+    $url = explode("/", $queryString); //Split the URL up into sections, delimited by the character '/' 
+    echo($url[0],$url[1],$url[2],$url[3],$url[4],$url[5]);
+    # echo('<script src=\"http://api.npr.org/query?id='$url[2];'&searchTerm='$url[3];'&output='$url[4];'&apiKey='$url[5];'&callback='$url[6];'\" type=\"text/javascript\"></script>');
+*/    
+?>
+    
+</body>
+</html>
